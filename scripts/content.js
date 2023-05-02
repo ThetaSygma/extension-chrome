fd8e0a9ec26b2c7e0f122790fae13b9039bd088e
ThetaSygma <aymeric.d87@gmail.com>
2023-05-02 17:40:01 +0200

    oui

--- scripts/content.js
+++ scripts/content.js
@@ -1,56 +1,24 @@
-console.log("aa");
-window.addEventListener('load', function () {
-    let videoUrl;
-    let previousURL;
-    if (window.location.href != "https://www.youtube.com/") {
+function test() {
+    videoData = JSON.parse(localStorage.getItem('videoData')) || {}
+    const regex = /watch\?v=/;
+    if (regex.test(window.location.href)) {
         videoUrl = window.location.href;
         videoUrl = videoUrl.replace(/&.*/i, "");
     }
     if (videoUrl != previousURL) {
+        videoData[videoUrl] = (videoData[videoUrl] || 0) + 1;
         previousURL = videoUrl;
+        console.log('Données récupérées de localStorage :', videoData);
+
+        chrome.storage.local.set({ videoData }).then(() => {
+            console.log("Value is set to " + videoData[videoUrl]);
+        });
     }
-    setInterval(() => {
-
-        if (window.location.href != "https://www.youtube.com/") {
-            videoUrl = window.location.href;
-            videoUrl = videoUrl.replace(/&.*/i, "");
-        }
-        if (videoUrl != previousURL) {
-            previousURL = videoUrl;
-        }
-
-    }, 1000);
-});
+}
 
 let previousURL;
-videoData = JSON.parse(localStorage.getItem('videoData')) || {}
-if (window.location.href != "https://www.youtube.com/") {
-    videoUrl = window.location.href;
-    videoUrl = videoUrl.replace(/&.*/i, "");
-}
-if (videoUrl != previousURL) {
-    videoData[videoUrl] = (videoData[videoUrl] || 0) + 1;
-    previousURL = videoUrl;
-    console.log('Données récupérées de localStorage :', videoData);
-
-    chrome.storage.local.set({ videoData }).then(() => {
-        console.log("Value is set to " + value);
-    });
-}
-setInterval(() => {
-    videoData = JSON.parse(localStorage.getItem('videoData')) || {}
-    if (window.location.href != "https://www.youtube.com/") {
-        videoUrl = window.location.href;
-        videoUrl = videoUrl.replace(/&.*/i, "");
-
-        if (videoUrl != previousURL) {
-            videoData[videoUrl] = (videoData[videoUrl] || 0) + 1;
-            previousURL = videoUrl;
-            console.log('Données récupérées de localStorage :', videoData);
-
-            chrome.storage.local.set({ videoData }).then(() => {
-                console.log("Value is set to " + value);
-            });
-        }
-    }
-}, 1000);
+let videoUrl;
+let videoData;
+window.addEventListener("click", function () {
+    test();
+});
